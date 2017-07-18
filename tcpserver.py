6ac8420f907b9f0984692cda9283f0036149aebb
lin344902118 <344902118@qq.com>
2017-07-18 16:44:57 +0800

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -2,15 +2,15 @@ import socket
 import threading
 
 bind_ip = '0.0.0.0'
-bing_port = 9999
+bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind((bind_ip, bing_port))
+server.bind((bind_ip, bind_port))
 
 server.listen(5)
 
-print "[*] Listening on %s:%d" (bind_ip, bing_port)
+print "[*] Listening on %s:%d" (bind_ip, bind_port)
 
 def handle_client(client_socket):
 
